897f9e5ae5b3a136f1875e84e1cc91356197abce
Alexandru Loghin <alexandru.loghin@vspartners.us>
2022-12-23 18:23:52 +0200

    feat: connected to mongoDB and added schemas for launches and planets

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,25 @@ const app = require("./app");
 const cluster = require("cluster");
 const os = require("os");
 const { loadPlanetsData } = require("./models/planets.model");
+const mongoose = require("mongoose");
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:LT7bkcheKqH15XbW@cluster0.vl3gf4x.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
